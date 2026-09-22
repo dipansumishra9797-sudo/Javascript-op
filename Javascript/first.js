9f140f6897b4181c26ea3bb540a69014e85350b4
dipansumishra9797-sudo <dipansumishra9797@gmail.com>
2026-09-22 23:44:16 +0530

    array

--- Javascript/first.js
+++ Javascript/first.js
@@ -192,7 +192,12 @@ for(let city of cities){
 }
 
 let marks = [85,97,44,37,76,60];
+let sum = 0;
 for(let i = 0 ; i < marks.length; i++){
-    i = i + marks[i];
+    sum = sum + marks[i];
 }
-console.log(i);*/
+console.log(sum);
+let avg = sum/6;
+console.log(avg);*/
+
+
